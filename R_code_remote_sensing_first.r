334165ae3c32d4817003389d8400468d5689b13c
lindalorenzini <80513534+lindalorenzini@users.noreply.github.com>
2021-03-17 12:51:22 +0100

    Update R_code_remote_sensing_first.r

--- R_code_remote_sensing_first.r
+++ R_code_remote_sensing_first.r
@@ -1,11 +1,21 @@
 # My first R code for remote sensing
 
-setwd("C:/lab/") # Windows
+setwd("C:/lab/") # Windows, permette di scegliere la set working directory
 
-# install.packages("raster")
+# install.packages("raster"), seleziono il pacchetto da cui prendo gli oggetti, in questo caso immagini
 library(raster)
 
 p224r63_2011<-brick("p224r63_2011_masked.grd")
-p224r63_2011
+# scrivendo l'argomento di brick ottengo le informazioni sull'immagine
+p224r63_2011 
 
-plot(p224r63_2011)
+# funzione plot ci permette di visualizzare le immagini
+plot(p224r63_2011) #permette di visualizzare le immagini
+
+#colorRampPalette è una funzione che ci permette di scegliere la banda di colori in cui visualizzare l'immagine
+cl <- colorRampPalette(c("black","grey","light grey"))(100)
+plot(p224r63_2011, col =cl)
+
+#nuova palette di colori 
+cl <- colorRampPalette(c("blue","red","yellow"))(100)
+plot(p224r63_2011, col =cl)
