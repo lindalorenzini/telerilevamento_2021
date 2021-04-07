63bd9a739e5cdbd3a757be53ca5d0222cf645421
lindalorenzini <80513534+lindalorenzini@users.noreply.github.com>
2021-04-07 07:55:45 +0200

    Update R_code_time_series.r

--- R_code_time_series.r
+++ R_code_time_series.r
@@ -1,1 +1,17 @@
+#time series analysis
+#incremento della temperatura 
+#dati e codice di Manuela Cosma
+  
+# install.packages("raster")
+library(raster)
+#settiamo una nuova working directory
+# setwd("/Users/name/Desktop/lab/greenland") # Mac 
+# setwd("~/lab/greenland") # Linux
+setwd("C:/lab/greenland") # Windows
+
+install.packages("rasterVis") # attenzione alla maiuscola e virgolette perché usciamo da R
+
+
+
+
 
