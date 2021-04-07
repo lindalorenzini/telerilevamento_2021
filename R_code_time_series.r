6f3b51c9c1057ae092cef61d2ea35b63f5f928d1
lindalorenzini <80513534+lindalorenzini@users.noreply.github.com>
2021-04-07 13:04:55 +0200

    Update R_code_time_series.r

--- R_code_time_series.r
+++ R_code_time_series.r
@@ -1,6 +1,6 @@
 #time series analysis
 #incremento della temperatura 
-#dati e codice di Manuela Cosma
+#dati e codice di Emanuela Cosma
   
 # install.packages("raster")
 library(raster)
@@ -11,7 +11,29 @@ setwd("C:/lab/greenland") # Windows
 
 install.packages("rasterVis") # attenzione alla maiuscola e virgolette perché usciamo da R
 
+#la funzione raster serve a creare un oggetto con un layer raster, mi permette di caricare singoli dati invece che pacchetti di dati come fa la funzione brick
+lst_2000<-raster("lst_2000.tif")
+plot(lst_2000)
+lst_2005 <- raster("lst_2005.tif")
+plot(lst_2005) 
+lst_2010 <- raster("lst_2010.tif")
+lst_2015 <- raster("lst_2015.tif")
+#esercizio multiframe con le quattro immagini
 
+par(mfrow=c(2,2))
+plot(lst_2000)
+plot(lst_2005)
+plot(lst_2010)
+plot(lst_2015)
 
+#funzione lapply() permette di applicare una determinata funzione ad una lista di file
+#creiamo una lista di files con la funzione list.files
+rlist <- list.files(pattern="lst")
+rlist
+#una volta creata la lista posso applicarvi una funzione tramite lapply
+#applico la funzione raster sulla lista appena creata
+import <- lapply(rlist,raster)
+import
 
-
+TGr <- stack(import)
+plot(TGr)
