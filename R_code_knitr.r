6639dd33adcd397e654747140c754e4ad52de5cc
lindalorenzini <80513534+lindalorenzini@users.noreply.github.com>
2021-04-21 17:20:14 +0200

    Update R_code_knitr.r

--- R_code_knitr.r
+++ R_code_knitr.r
@@ -5,3 +5,5 @@ setwd("C:/lab/") # Windows
 #knitr è un pacchetto utile per creare file con diversi pdf all'interno
 # knitr prende un codice all'esterno di r, lo porta all'interno di r dove genera un report nella stessa cartella del codice
 library(knitr)
+#funzione stitch: il primo argomento da inserire è come si chiama il nostro codice
+stitch("R_code_greenland.r.docx", template=system.file("misc", "knitr-template.Rnw", package="knitr"))
