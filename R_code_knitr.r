540e9142a3a5fb4369c1f86e07a9c0e9373d18b2
lindalorenzini <80513534+lindalorenzini@users.noreply.github.com>
2021-04-21 15:14:16 +0200

    Create R_code_knitr.r

---
+++ R_code_knitr.r
@@ -0,0 +1,7 @@
+#R_code_knitr.r
+
+#prima cosa settiamo la working directory
+setwd("C:/lab/") # Windows
+#knitr è un pacchetto utile per creare file con diversi pdf all'interno
+# knitr prende un codice all'esterno di r, lo porta all'interno di r dove genera un report nella stessa cartella del codice
+library(knitr)
