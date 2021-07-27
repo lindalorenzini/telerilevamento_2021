99ce245979c6c3cfb06c9fbd8195d7f75ec1096e
lindalorenzini <80513534+lindalorenzini@users.noreply.github.com>
2021-07-27 16:04:06 +0200

    Update R_code_progetto.r

--- R_code_progetto.r
+++ R_code_progetto.r
@@ -1,14 +1,10 @@
-# Mountain top mining in Virginia
+# Mountain top mining in West Virginia
 
 library(raster)
 setwd("C:/lab/mountaintopmining")
 mining84<-brick("hobet_19840917.jpg") 
-mining88<-brick("hobet_19880827.jpg")
-mining92<-brick("hobet_19920806.jpg")
 mining96<-brick("hobet_19961004.jpg")
-mining00<-brick("hobet_20001015.jpg")
 mining04<-brick("hobet_20040706.jpg")
-mining08<-brick("hobet_20080818.jpg")
 mining12<-brick("hobet_20120920.jpg")
 mining84
 # ho un raster brick in 3 bande: banda1=hobet_19840917.1, banda2= hobet_19840917.2, banda3= hobet_19840917.3
