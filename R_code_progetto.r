8b6bcf8060208e96bfa9d747e53c016d4f5b0314
lindalorenzini <80513534+lindalorenzini@users.noreply.github.com>
2021-07-30 11:05:28 +0200

    Update R_code_progetto.r

--- R_code_progetto.r
+++ R_code_progetto.r
@@ -54,18 +54,30 @@ setwd("C:/lab/mountaintopmining")
 mining84<-brick("hobet_19840917.jpg") 
 plotRGB(mining84, r=1, g=2, b=3, stretch="lin")
 ggRGB(mining84, r=1, g=2, b=3, stretch="lin")
-
+ 
+mining96<-brick("hobet_19961004.jpg")
+plotRGB(mining96, r=1, g=2, b=3, stretch="lin")
+ggRGB(mining96, r=1, g=2, b=3, stretch="lin")
+ 
+mining04<-brick("hobet_20040706.jpg")
+plotRGB(mining04, r=1, g=2, b=3, stretch="lin")
+ggRGB(mining04, r=1, g=2, b=3, stretch="lin")
+ 
 mining12<-brick("hobet_20120920.jpg")
 plotRGB(mining12, r=1, g=2, b=3, stretch="lin")
 ggRGB(mining12, r=1, g=2, b=3, stretch="lin")
 
-par(mfrow=c(2,1)) 
+par(mfrow=c(2,2)) 
 plotRGB(mining84, r=1, g=2, b=3, stretch="lin")
+plotRGB(mining96, r=1, g=2, b=3, stretch="lin")
+plotRGB(mining04, r=1, g=2, b=3, stretch="lin")
 plotRGB(mining12, r=1, g=2, b=3, stretch="lin")
 
 picture1 <- ggRGB(mining84, r=1, g=2, b=3, stretch="lin")
-picture2 <- ggRGB(mining12, r=1, g=2, b=3, stretch="lin")
-grid.arrange(picture1, picture2, nrow=2)
+picture2 <- ggRGB(mining96, r=1, g=2, b=3, stretch="lin")
+picture3 <- ggRGB(mining04, r=1, g=2, b=3, stretch="lin")
+picture4 <- ggRGB(mining12, r=1, g=2, b=3, stretch="lin")
+grid.arrange(picture1, picture2, picture3, picture4, nrow=2)
 
 m84c <- unsuperClass(mining84, nClasses=2)
 m84c
@@ -81,6 +93,38 @@ m84c
 #names      : layer 
 #values     : 1, 2  (min, max)
 plot(m84c$map)
+
+m96c <- unsuperClass(mining96, nClasses=2)
+m96c
+#unsuperClass results
+
+#*************** Map ******************
+#$map
+#class      : RasterLayer 
+#dimensions : 480, 720, 345600  (nrow, ncol, ncell)
+#resolution : 1, 1  (x, y)
+#extent     : 0, 720, 0, 480  (xmin, xmax, ymin, ymax)
+#crs        : NA 
+#source     : memory
+#names      : layer 
+#values     : 1, 2  (min, max)
+plot(m96c$map)
+
+m04c <- unsuperClass(mining04, nClasses=2)
+m04c
+#unsuperClass results
+
+#*************** Map ******************
+#$map
+#class      : RasterLayer 
+#dimensions : 480, 720, 345600  (nrow, ncol, ncell)
+#resolution : 1, 1  (x, y)
+#extent     : 0, 720, 0, 480  (xmin, xmax, ymin, ymax)
+#crs        : NA 
+#source     : memory
+#names      : layer 
+#values     : 1, 2  (min, max)
+plot(m04c$map)
 
 m12c <- unsuperClass(mining12, nClasses=2)
 m12c
@@ -112,15 +156,46 @@ prop1
 
 # 1984-> 92,3% foresta, 7,7% miniera
 
+freq(m96c$map)
+#  value  count
+#[1,]     1 298127 foresta
+#[2,]     2  47473 miniera
+sum2<- 298127 + 47473
+sum2
+#sum2= 345600
+prop2 <- freq(m96c$map) / sum2
+prop2
+#            value    count
+#[1,] 2.893519e-06 0.862636
+#[2,] 5.787037e-06 0.137364
+
+# 1996-> 86,3% foresta, 13,7% miniera
+
+freq(m04c$map)
+#     value  count
+#[1,]     1  34232 miniera
+#[2,]     2 311368 foresta
+
+sum3<- 311368+ 34232
+sum3
+#sum2= 345600
+prop3 <- freq(m04c$map) / sum3
+prop3
+#            value      count
+#[1,] 2.893519e-06 0.09905093
+#[2,] 5.787037e-06 0.90094907
+
+#2004-> 90,1% foresta, 9,9% miniera
+
 freq(m12c$map)
 # value  count
 #[1,]     1  297548 foresta
 #[2,]     2 48052 miniera
-sum2<-297548 + 48052
-sum2
+sum4<-297548 + 48052
+sum4
 #sum2= 345600
-prop2 <- freq(m12c$map) / sum2
-prop2
+prop4 <- freq(m12c$map) / sum4
+prop4
 #         value    count
 #[1,] 2.893519e-06 0.8609606 foresta 
 #[2,] 5.787037e-06 0.1390394 miniera
@@ -130,26 +205,31 @@ prop2
 #inseriamo i fattori che sono variabili categoriche, nel nostro caso foresta e zone agricole, inseriamo le percentuali di entrambe le classi, otterremo una tabella a 3 colonne
 cover <- c("Forest","Mine")
 percent_1984 <- c(92.3, 7.7)
+percent_1996 <- c(86.3,13.7)
+percent_2004 <- c (90.1, 9.9)
 percent_2012 <- c(86.1, 13.9)
 
 
 #generiamo il dataframe con la funzione data.frame e diamo un nome e infine lo visualizziamo
 
-percentages <- data.frame(cover, percent_1984, percent_2012)
+percentages <- data.frame(cover, percent_1984, percent_1996, percent_2004, percent_2012)
 percentages 
-#  cover percent_1984 percent_2012
-#1 Forest         92.3         86.1
-#2   Mine          7.7         13.9
+#        cover percent_1984   percent_1996   percent_2004   percent_2012
+#1 Forest         92.3           86.3           90.1           86.1
+#2   Mine          7.7           13.7            9.9           13.9
 
-#generiamo un grafico con ggplot, la sintassi è particolare 
+#generiamo i grafici con ggplot, la sintassi è particolare 
 ggplot(percentages, aes(x=cover, y=percent_1984, color=cover)) + geom_bar(stat="identity", fill="white")
-#ripetismo l'operazione del ggplot per il 2006
+ggplot(percentages, aes(x=cover, y=percent_1996, color=cover)) + geom_bar(stat="identity", fill="white")
+ggplot(percentages, aes(x=cover, y=percent_2004, color=cover)) + geom_bar(stat="identity", fill="white")
 ggplot(percentages, aes(x=cover, y=percent_2012, color=cover)) + geom_bar(stat="identity", fill="white")
 #associamo i plot generati a un nome
 graf1<- ggplot(percentages, aes(x=cover, y=percent_1984, color=cover)) + geom_bar(stat="identity", fill="white")
-graf2 <- ggplot(percentages, aes(x=cover, y=percent_2012, color=cover)) + geom_bar(stat="identity", fill="white")
+graf2 <- ggplot(percentages, aes(x=cover, y=percent_1996, color=cover)) + geom_bar(stat="identity", fill="white")
+graf3 <- ggplot(percentages, aes(x=cover, y=percent_2004, color=cover)) + geom_bar(stat="identity", fill="white")
+graf4 <- ggplot(percentages, aes(x=cover, y=percent_2012, color=cover)) + geom_bar(stat="identity", fill="white")
 #utilizziamo grid.arrange per unire i ggplot ottenuti occorre avare la library gridExtra, otteniamo così i due grafici delle due annate si può così lavorare sui cambiamenti percentuali nel tempo
-grid.arrange(graf1, graf2, nrow=1)
+grid.arrange(graf1, graf2, graf3, graf4, nrow=1)
 
 
 
