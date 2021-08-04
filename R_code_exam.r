1829c28e548d0ba09407f243c1231224fbfa2087
lindalorenzini <80513534+lindalorenzini@users.noreply.github.com>
2021-08-04 11:20:09 +0200

    Update R_code_exam.r

--- R_code_exam.r
+++ R_code_exam.r
@@ -71,6 +71,8 @@ mining12
 # bande mining12: banda1= hobet_20120920.1, banda2= hobet_20120920.2 , banda3= hobet_20120920.3
 
 #per vedere le immagini con i colori della banda del visibile le vado a plottare in RGB
+# R= red, G= green, B= blue
+# associo ognuno dei 3 colori alle diverse bande della mia immagine (r=1, g=2, b=3) e scelgo il tipo di stretch (stretch= "Lin")
 plotRGB(mining84, r=1,g=2,b=3, stretch="Lin")
 plotRGB(mining96, r=1,g=2,b=3, stretch="Lin")
 plotRGB(mining04, r=1,g=2,b=3, stretch="Lin")
@@ -83,26 +85,45 @@ plotRGB(mining96, r=1,g=2,b=3, stretch="Lin")
 plotRGB(mining04, r=1,g=2,b=3, stretch="Lin")
 plotRGB(mining12, r=1,g=2,b=3, stretch="Lin")
 
+#salvo l'immagine ottenuta con la funzione pdf()
+pdf("plotRGB.pdf")
+par(mfrow=c(2,2)) 
+plotRGB(mining84, r=1,g=2,b=3, stretch="Lin")
+plotRGB(mining96, r=1,g=2,b=3, stretch="Lin")
+plotRGB(mining04, r=1,g=2,b=3, stretch="Lin")
+plotRGB(mining12, r=1,g=2,b=3, stretch="Lin")
+dev.off()
+
 #time series
-# per evitare di importare singolarmente i files, creo una lista dei files di interesse 
+
+# per evitare di importare singolarmente i files, creo una lista dei files di interesse e li associo ad un oggetto (rlist) per facilitare le funzioni successive 
 rlist <- list.files(pattern="hobet")
 rlist
+#[1] "hobet_19840917.jpg" "hobet_19961004.jpg" "hobet_20040706.jpg"
+#[4] "hobet_20120920.jpg"
 
 # a questo punto importo la lista di files tramite la funzione lapply() che mi permette di applicare una funzione, nel nostro caso la funzione raster, ad una lista di files precedentemente creata, nel nostro caso rlist
 import <- lapply(rlist,raster)
 import
 
-# md =  hobet mine development
+# hmd =  hobet mine development
+# funzione stack permette di concatenare diversi vettori a formarne uno unico
 hmd <- stack(import)
 plot(hmd)
 
 levelplot(hmd)
 levelplot(hmd$hobet_19840917)
 
+#cambio la colorRampPalette in modo da identificare meglio le zone di differenziazione 
 cl <- colorRampPalette(c("blue","light blue","pink","red"))(100)
-levelplot(hmd, col.regions=cl)
-levelplot(hmd,col.regions=cl, names.attr=c("1984","1996", "2004", "2012"))
+# nel levelplot le parti tendenti al rosso andranno ad identificare le parti di suolo nudo rappresentati dalla miniera, l'azzurro invece identifica le zone di copertura vegetale 
 levelplot(hmd,col.regions=cl, main="Hobet coal mine (West Virginia) development",names.attr=c("1984","1996", "2004", "2012"))
+
+#salvo l'immagine risultante dalla time series come pdf 
+pdf("timeseries1894_2012.pdf")
+levelplot(hmd,col.regions=cl, main="Hobet coal mine (West Virginia) development",names.attr=c("1984","1996", "2004", "2012"))
+dev.off()
+
 
 #land cover: differenze tra 1984 e 2012
 library(raster)
@@ -110,28 +131,10 @@ library(RStoolbox)
 library(ggplot2)
 library(gridExtra)
 setwd("C:/lab/mountaintopmining")
-
 mining84<-brick("hobet_19840917.jpg") 
-plotRGB(mining84, r=1, g=2, b=3, stretch="lin")
-ggRGB(mining84, r=1, g=2, b=3, stretch="lin")
- 
 mining96<-brick("hobet_19961004.jpg")
-plotRGB(mining96, r=1, g=2, b=3, stretch="lin")
-ggRGB(mining96, r=1, g=2, b=3, stretch="lin")
- 
 mining04<-brick("hobet_20040706.jpg")
-plotRGB(mining04, r=1, g=2, b=3, stretch="lin")
-ggRGB(mining04, r=1, g=2, b=3, stretch="lin")
- 
 mining12<-brick("hobet_20120920.jpg")
-plotRGB(mining12, r=1, g=2, b=3, stretch="lin")
-ggRGB(mining12, r=1, g=2, b=3, stretch="lin")
-
-par(mfrow=c(2,2)) 
-plotRGB(mining84, r=1, g=2, b=3, stretch="lin")
-plotRGB(mining96, r=1, g=2, b=3, stretch="lin")
-plotRGB(mining04, r=1, g=2, b=3, stretch="lin")
-plotRGB(mining12, r=1, g=2, b=3, stretch="lin")
 
 picture1 <- ggRGB(mining84, r=1, g=2, b=3, stretch="lin")
 picture2 <- ggRGB(mining96, r=1, g=2, b=3, stretch="lin")
@@ -143,6 +146,7 @@ grid.arrange(picture1, picture2, picture3, picture4, nrow=2)
 set.seed(1)
 # funzione rnorm(): normalizza i valori dei pixel fissando la classe scelta
 rnorm(1)
+
 m84c <- unsuperClass(mining84, nClasses=2)
 m84c
 #unsuperClass results
@@ -217,6 +221,7 @@ plot(m84c$map)
 plot(m96c$map)
 plot(m04c$map)
 plot(m12c$map)
+dev.off()
 
 freq(m84c$map)
 # value  count
@@ -280,23 +285,21 @@ prop4
 
 #2012-> 86,1% foresta, 13,9% miniera
 
-#inseriamo i fattori che sono variabili categoriche, nel nostro caso foresta e zone agricole, inseriamo le percentuali di entrambe le classi, otterremo una tabella a 3 colonne
+#inseriamo i fattori che sono variabili categoriche, nel nostro caso foresta e miniera, inseriamo le percentuali di entrambe le classi, otterremo una tabella a 3 colonne
 cover <- c("Forest","Mine")
 percent_1984 <- c(92.3, 7.7)
 percent_1996 <- c(86.3,13.7)
 percent_2004 <- c (90.1, 9.9)
 percent_2012 <- c(86.1, 13.9)
 
-
 #generiamo il dataframe con la funzione data.frame e diamo un nome e infine lo visualizziamo
-
 percentages <- data.frame(cover, percent_1984, percent_1996, percent_2004, percent_2012)
 percentages 
 #    cover        percent_1984   percent_1996   percent_2004   percent_2012
 #1   Forest         92.3           86.3           90.1           86.1
 #2   Mine            7.7           13.7            9.9           13.9
 
-#generiamo i grafici con ggplot, la sintassi è particolare 
+#generiamo i grafici con ggplot 
 ggplot(percentages, aes(x=cover, y=percent_1984, fill=cover)) + geom_bar(stat="identity", color="white")
 ggplot(percentages, aes(x=cover, y=percent_1996, fill=cover)) + geom_bar(stat="identity", color="white")
 ggplot(percentages, aes(x=cover, y=percent_2004, fill=cover)) + geom_bar(stat="identity", color="white")
@@ -308,8 +311,24 @@ graf2 <- ggplot(percentages, aes(x=cover, y=percent_1996, fill=cover)) + geom_ba
 graf3 <- ggplot(percentages, aes(x=cover, y=percent_2004, fill=cover)) + geom_bar(stat="identity", color="white")
 graf4 <- ggplot(percentages, aes(x=cover, y=percent_2012, fill=cover)) + geom_bar(stat="identity", color="white")
 
-#utilizziamo grid.arrange per unire i ggplot ottenuti occorre avare la library gridExtra, otteniamo così i due grafici delle due annate si può così lavorare sui cambiamenti percentuali nel tempo
+#utilizziamo grid.arrange per unire i ggplot ottenuti, occorre avere la library gridExtra, otteniamo così i 4 grafici delle 4 annate per analizzare i cambiamenti percentuali nel tempo
 grid.arrange(graf1, graf2, graf3, graf4, nrow=1)
+
+pdf("graficiggplot_vers1.pdf")
+grid.arrange(graf1, graf2, graf3, graf4, nrow=1)
+dev.off()
+
+graf5 <- ggplot(percentages, aes(x=cover, y=percent_1984, fill=cover))  + ggtitle("Cover percentage 1984") + geom_bar(stat="identity", color= "black") + scale_fill_manual(values=c("seagreen", "wheat"))
+graf6 <- ggplot(percentages, aes(x=cover, y=percent_1996, fill=cover))+ ggtitle("Cover percentage 1996") + geom_bar(stat="identity", color= "black") + scale_fill_manual(values=c("seagreen", "wheat"))
+graf7 <- ggplot(percentages, aes(x=cover, y=percent_2004, fill=cover)) + ggtitle("Cover percentage in 2004") + geom_bar(stat="identity", color= "black") + scale_fill_manual(values=c("seagreen", "wheat"))
+graf8 <- ggplot(percentages, aes(x=cover, y=percent_2012, fill=cover)) + ggtitle("Cover percentage in 2012") + geom_bar(stat="identity", color= "black") + scale_fill_manual(values=c("seagreen", "wheat"))
+grid.arrange(graf5, graf6, graf7, graf8, nrow=1)
+
+pdf("graficiggplot_vers2.pdf")
+grid.arrange(graf5, graf6, graf7, graf8, nrow=1)
+dev.off()
+
+
 
 
 ggplot(increasedpercentages, aes(x=MSHcover, y=coverpercentagein1987, fill=MSHcover)) + ggtitle("Cover percentage on the volcanic slopes of Mount Saint Helens in 1987") + theme(plot.title=element_text(face="bold")) + scale_x_discrete(limits = idealgraphicsuccession) + scale_y_continuous(limits = c(min(0),max(71))) + geom_bar(stat="identity") + theme(legend.position="bottom") + scale_fill_manual(values=c("gold", "burlywood3", "darkolivegreen4"))
